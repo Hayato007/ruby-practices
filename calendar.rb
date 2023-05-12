920d1108f961f6e5810ec6507c5f336578820fb0
hayato <h-endo@ruby-dev.jp>
2023-05-12 10:35:59 +0900

    add calendar

---
+++ calendar.rb
@@ -0,0 +1,47 @@
+require 'optparse'
+require 'date'
+
+options = {}
+OptionParser.new do |opts|
+  opts.banner = "Usage: cal.rb [options]"
+
+  opts.on("-m", "--month MONTH", Integer, "Specify month (1-12)") do |month|
+    options[:month] = month
+  end
+
+  opts.on("-y", "--year YEAR", Integer, "Specify year (1970-2100)") do |year|
+    options[:year] = year
+  end
+end.parse!
+
+if options[:year] && !options[:month]
+  puts "Error: Month is required when specifying a year."
+  exit
+end
+
+today = Date.today
+year = options[:year] || today.year
+month = options[:month] || today.month
+
+begin
+  date = Date.new(year, month, 1)
+rescue ArgumentError
+  puts "Error: Invalid year or month specified."
+  exit
+end
+
+last_day = Date.new(year, month, -1).day
+
+puts "#{Date::MONTHNAMES[month]}, #{year}".center(20)
+puts "Su Mo Tu We Th Fr Sa"
+
+(1..last_day).each do |day|
+  print day.to_s.rjust(2) + " "
+
+  if date.wday == 6 || day == last_day
+    print "\n" if day != last_day
+    date += (7 - date.wday) % 7
+  else
+    date += 1
+  end
+end
