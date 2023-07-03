8016ecdf0a26bc5f836e7909ed4b014e7490cb15
hayato <h-endo@ruby-dev.jp>
2023-07-03 15:29:04 +0900

    Enumerable#sum を使用して改修

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -17,20 +17,24 @@ shots.each do |s|
   end
 end
 
-point = 0 # 合計点を格納する変数
-10.times do |n|
+point = 0
+point1 = 0
+
+point = 10.times do |n|
   frame = frames[n] # 現在のフレームのスコア
   next_frame = frames[n + 1] || [] # 次のフレームのスコア（存在しない場合は空配列）
   after_next_frame = frames[n + 2] || [] # 次の次のフレームのスコア（存在しない場合は空配列）
   left_shots = next_frame + after_next_frame # 残りの投球のスコア
 
-  point += frame.sum # 現在のフレームのスコアを合計点に加算
+  point1 += frame.sum # 現在のフレームのスコアを合計点に加算
 
   if frame[0] == 10 # ストライクの場合
-    point += (left_shots.slice(0, 2)).sum # 次の2投のスコアを加算
+    point1 += (left_shots.slice(0, 2)).sum # 次の2投のスコアを加算
   elsif frame.sum == 10 # スペアの場合
-    point += (next_frame[0] || 0) # 次の1投のスコアを加算
+    point1 += (next_frame[0] || 0) # 次の1投のスコアを加算
   end
 end
 
 puts point  # 合計点を表示
+puts point1  # 合計点を表示
+
