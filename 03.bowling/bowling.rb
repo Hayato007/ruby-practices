fc227a098d0c00ac6280d0fb7d770949c3778606
hayato <h-endo@ruby-dev.jp>
2023-07-07 17:06:03 +0900

    ポイント計算に対する全体的な修正

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -8,6 +8,7 @@ frame = [] # 現在のフレームのスコアを一時的に格納する配列
 
 shots.each do |s|
   frame << s # スコアを現在のフレームに追加
+
   if frames.length < 10 # 最後のフレーム以外の場合の処理
     if frame.length >= 2 || s == 10 # フレームが2投されたか、ストライクの場合
       frames << frame.dup # フレームを追加し、新しいフレームを開始
@@ -18,21 +19,77 @@ shots.each do |s|
   end
 end
 
-point1 = 0
 
-point = 10.times do |n|
+point = 0
+
+point = 10.times.sum do |n|
   frame = frames[n] # 現在のフレームのスコア
-  next_frame = frames[n + 1] || [] # 次のフレームのスコア（存在しない場合は空配列）
-  after_next_frame = frames[n + 2] || [] # 次の次のフレームのスコア（存在しない場合は空配列）
-  left_shots = next_frame + after_next_frame # 残りの投球のスコア
-
-  point1 += frame.sum # 現在のフレームのスコアを合計点に加算
-
-  if frame[0] == 10 # ストライクの場合
-    point1 += (left_shots.slice(0, 2)).sum # 次の2投のスコアを加算
-  elsif frame.sum == 10 # スペアの場合
-    point1 += (next_frame[0] || 0) # 次の1投のスコアを加算
+  if n < 9 # 最終フレーム以外
+    if frame[0] == 10 # ストライクの場合
+      if frames[n + 1] && frames[n + 2] ## 9フレーム目と10フレームがXの場合9フレームは11フレームを探してしまうので、それを抑制するコードである。
+        if frames[n + 1][0] != 10 # ２フレーム目がストライクでない場合のコードを追加しなくてはいけない！！！！！！！！！# ストライク一回だけの場合は次フレームの合計のみを取得すれば良い
+         frame.sum + frames[n + 1].sum
+        else  #１フレームと2フレーム目がストライクの場合、３フレーム目の情報が必要
+          frame.sum + frames[n + 1].sum + frames[n + 2][0] # ストライクの場合、次のフレームの投球を取得しなければいけないが,２フレーム先まで取得してしまっているため、点数に差異が生じる。
+        end
+      else
+        frame.sum + frames[n + 1][0] + frames[n + 1][1]
+      end
+    elsif frame.sum == 10 # スペアの場合
+      frame.sum + frames[n + 1][0]
+    else # 最終フレームの場合
+      frame.sum
+    end
+  else # 最終フレームの場合
+    frame.sum
   end
 end
 
-puts point1  # 合計点を表示
+puts point # 合計点を表示
+# point = 0
+
+# point = 10.times.sum do |n|
+#   frame = frames[n] # 現在のフレームのスコア
+#   if n < 9 # 最終フレーム以外
+#     if frame[0] == 10 # ストライクの場合
+#       if frames[n + 1] && frames[n + 2] ## 9フレーム目と10フレームがXの場合9フレームは11フレームを探してしまうので、それを抑制するコードである。
+#         if frames[n + 1][0] != 10 # ２フレーム目がストライクでない場合のコードを追加しなくてはいけない！！！！！！！！！# ストライク一回だけの場合は次フレームの合計のみを取得すれば良い
+#          frame.sum + frames[n + 1].sum
+#         else  #１フレームと2フレーム目がストライクの場合、３フレーム目の情報が必要
+#           frame.sum + frames[n + 1].sum + frames[n + 2].sum # ストライクの場合、次のフレームの投球を取得しなければいけないが,２フレーム先まで取得してしまっているため、点数に差異が生じる。
+#         end
+#       end
+#     elsif frame.sum == 10 # スペアの場合
+#       frame.sum + frames[n + 1][0]
+#     else # 最終フレームの場合
+#       frame.sum
+#     end
+#   end
+# end
+
+# point = 10.times.sum do |n|
+#   frame = frames[n] # 現在のフレームのスコア
+#   if n < 9 # 最終フレーム以外
+#     if frame[0] == 10 # ストライクの場合
+#       if frames[n + 1] && frames[n + 2] ## 9フレーム目と10フレームがXの場合9フレームは11フレームを探してしまうので、それを抑制するコードである。
+#         # if frame[n + 1] =! 10 # ２フレーム目がストライクでない場合のコードを追加しなくてはいけない！！！！！！！！！
+#         #                 # ストライク一回だけの場合は次フレームの合計のみを取得すれば良い
+#         # else                #１フレームと2フレーム目がストライクの場合、３フレーム目の情報が必要
+#         frame.sum + frames[n + 1].sum + frames[n + 2].sum
+#         # ストライクの場合、次のフレームの投球を取得しなければいけないが、
+#         # ２フレーム先まで取得してしまっているため、点数に差異が生じる。
+#         # end
+#       else
+#         frame.sum
+#       end
+#     elsif frame.sum == 10 # スペアの場合
+#       frame.sum + frames[n + 1][0]
+#     else
+#       frame.sum
+#     end
+#   else # 最終フレームの場合
+#     frame.sum
+#   end
+# end
+
+# puts point # 合計点を表示
