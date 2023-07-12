05ae662775fc663d485e8a02dced4d0f1df4daba
hayato <h-endo@ruby-dev.jp>
2023-07-12 16:35:46 +0900

    条件分岐内の修正

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -19,30 +19,29 @@ shots.each do |s|
   end
 end
 
-
 point = 0
 
 point = 10.times.sum do |n|
-  frame = frames[n] # 現在のフレームのスコア
-  if n < 9 # 最終フレーム以外
-    if frame[0] == 10 # ストライクの場合
-      if frames[n + 1] && frames[n + 2] ## 9フレーム目と10フレームがXの場合9フレームは11フレームを探してしまうので、それを抑制するコードである。
-        if frames[n + 1][0] != 10 # ２フレーム目がストライクでない場合のコードを追加しなくてはいけない！！！！！！！！！# ストライク一回だけの場合は次フレームの合計のみを取得すれば良い
-         frame.sum + frames[n + 1].sum
-        else  #１フレームと2フレーム目がストライクの場合、３フレーム目の情報が必要
-          frame.sum + frames[n + 1].sum + frames[n + 2][0] # ストライクの場合、次のフレームの投球を取得しなければいけないが,２フレーム先まで取得してしまっているため、点数に差異が生じる。
-        end
-      else
-        frame.sum + frames[n + 1][0] + frames[n + 1][1]
-      end
-    elsif frame.sum == 10 # スペアの場合
-      frame.sum + frames[n + 1][0]
-    else # 最終フレームの場合
-      frame.sum
-    end
-  else # 最終フレームの場合
+  frame = frames[n] 
+  if frame.sum < 10
     frame.sum
+  elsif frame[0] == 10 # ストライクの場合
+    if n ==9
+      frame.sum
+    elsif n == 8
+      frame.sum + frames[n + 1][0] + frames[n + 1][1]
+    elsif frames[n + 1][0] != 10
+      frame.sum + frames[n + 1].sum
+    else
+      frame.sum + frames[n + 1][0] + frames[n + 2][0]
+    end
+  elsif frame.sum >= 10 # スペアの場合もしくは、１０以上
+    if n < 9
+      frame.sum + frames[n + 1][0] # n=8の場合ここに入る
+    else
+      frame.sum # 最終フレームの場合ここに入る
+    end
   end
 end
 
-puts point # 合計点を表示
+puts point # 最終点
